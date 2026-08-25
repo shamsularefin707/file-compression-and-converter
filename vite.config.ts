4341f0770e8c1c50d455b216441c47fceae5d055
Md. Shamsul Arefin <mdshamsularefin707@gmail.com>
2026-08-25 11:39:28 +0600

    Revert base URL to root for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/file-compression-and-converter/',
   plugins: [react(), tailwindcss()],
 })
